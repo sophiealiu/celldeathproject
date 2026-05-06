10211777ca1863e04ff0a1088f9df1c40062c5b4
sophiealiu <saliu@mdanderson.org>
2026-05-06 12:42:02 -0500

    Update processing_visium.R

--- processing_visium.R
+++ processing_visium.R
@@ -1,5 +1,5 @@
 # This file uses isolated control vs. experimental before DE on pseudobulked data
-# combining the spatial coordinates of the bins and the gene expression for each of them
+# combining the spatial coordinates of the gene expression of our 19060 genes for all 367559 bins
 # saveRDS(object, file = file.path(inputdir, "Seurat objects", "obj.rds"))
 # write.csv(object, file.path(dir, "obj.csv"))
 
