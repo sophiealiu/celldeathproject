5a6636a7d8aa82b3e07f44f520b9854b8e223137
Sophie Liu <saliu@mdanderson.org>
2026-06-11 18:00:23 -0500

    Update 1.2-NMFs.R

--- 1.2-NMFs.R
+++ 1.2-NMFs.R
@@ -1,13 +1,13 @@
 # -----------------------------------------------------------------------------
 # Author: Sophie A. Liu
-# Date : 05/25/2026 
+# Date : 06/11/2026 5:58pm
 # Purpose: Creating NMF factors from Visium data and optimizing for downstream
 # -----------------------------------------------------------------------------
 # X ≈ WH (W is feature matrix ie. gene components, H is coefficient matrix ie. weights)
 
 library(Matrix)
 library(ggplot2)
-library(RcppML)        # faster computationally but treats it as sparse matrix
+library(RcppML)                # RcppML (and Gene NMF) for sparse matrices
 library(Seurat)
 
 # importing files
@@ -18,7 +18,7 @@ pd1_raw <- readRDS(file = file.path(datadir, "Seurat objects", "pd1_raw.rds"))
 
 
 # -----------------------------------------------------------------------------
-# 1. Expression matrix X before decomposing. merging here because raw unable to get matrix
+# 1. expression matrix X before decomposing. merging here because raw unable
 mat_iso <- GetAssayData(
   iso_raw,
   assay = "Spatial.008um",
@@ -35,137 +35,90 @@ mat_pd1 <- GetAssayData(
 colnames(mat_iso)  <- paste0("iso_", colnames(mat_iso))
 colnames(mat_pd1) <- paste0("pd1_", colnames(mat_pd1))
 
-# removing invalid/empty columns
-mat_iso <- mat_iso[, Matrix::colSums(mat_iso) > 0]
-mat_pd1 <- mat_pd1[, Matrix::colSums(mat_pd1) > 0]
-
-# merging genes
-all_genes <- union(rownames(mat_iso), rownames(mat_pd1))
-
-
-# allowing zeroes: if gene A doesn't exist in one condition, still keep relevance
-iso_full <- Matrix(0,
-  nrow = length(all_genes), ncol = ncol(mat_iso),
-  sparse = TRUE
-)
-
-pd1_full <- Matrix(0,
-  nrow = length(all_genes), ncol = ncol(mat_pd1),
-  sparse = TRUE
-)
-
-# have to match names back
-rownames(iso_full) <- rownames(mat_iso)
-rownames(pd1_full) <- rownames(mat_pd1)
-
-colnames(iso_full) <- colnames(mat_iso)
-colnames(pd1_full) <- colnames(mat_pd1)
-
-# final merged matrix
-merged_mat <- cbind(iso_full, pd1_full)
+merged_mat <- cbind(mat_iso, mat_pd1)
 
 
 # -----------------------------------------------------------------------------
-# 2. testing different k-ranks to optimize least error. when MSE improvement flattens 
-# ideally this also is stable with 30 runs
+# 2. testing k-ranks
+# a. avg loss across runs gives us optimal rank at curvature
 library(inflection)
+ks <- 2:20
+n <- 10                                              # 10 for estimation
 
-ks <- 2:50
-fit <- list()
-merged_mse <- list()
+loss_matrix <- matrix(nrow = length(ks), ncol = n)
 
 for (i in seq_along(ks)) {
-  k <- ks[i]
-  fit[[i]] <- nmf(merged_mat, k = k, verbose = FALSE)
-  
-  merged_mse[[i]] <- mse(merged_mat, 
-                         fit[[i]]$w, fit[[i]]$d, fit[[i]]$h, 
-                         mask_zeros = FALSE)
+  for (r in 1:n) {                                   # 7 is my favorite number
+    model <- nmf(merged_mat, k = i, seed = 7*n, verbose = FALSE)      
+    loss_matrix[i, r] <- mse(merged_mat, model$w, 
+                                         model$d, 
+                                         model$h)
+  }
 }
 
-names(merged_mse) <- ks
+mean_loss <- rowMeans(loss_matrix)
+k_opt <- uik(x = ks, y = mean_loss)
 
-merged_mseNum <- as.numeric(merged_mse)
-delta <- diff(merged_mseNum)
+# b. stability score at optimal rank
+n3 <- 30                                             # here, CLT
+Ws <- vector("list", n3)
+for (i in 1:n3) {
+  mod_stab <- nmf(merged_mat, k = k_opt, seed = 7*i, verbose = FALSE)
+  Ws[[i]] <- mod_stab$w
+}
 
-plot(2:50, delta, type = "b",          # begins from plotting change 2 -> 3
-     xlab = "k",
-     ylab = "change in MSE")
+pair_stab <- c()
+
+for (i in 1:(n - 1)) {
+  for (j in (i + 1):n) {
+    # pearson correlation between runs i& j. want close to 1
+    cor_mat <- cor(W_list[[i]], W_list[[j]])
+    
+    matched_corrs <- numeric(ks)
+    temp_mat <- cor_mat
+    
+    # greedy matching computationally more feasible. sorry to marriage problem
+    for (f in 1:ks) {
+      max <- which(temp_mat == max(temp_mat), arr.ind = TRUE)[1, ]
+      matched_corrs[f] <- temp_mat[max_idx[1], max_idx[2]]
+      temp_mat[max_idx[1], ] <- -1
+      temp_mat[, max_idx[2]] <- -1
+    }
+    
+    pair_stab <- c(pair_stab, mean(matched_corrs))
+  }
+}
+
+final_stab <- mean(pair_stab)
 
 
 # -----------------------------------------------------------------------------
-# 3. QC of NMFs by re-seeding. from optimization, k = 22
-n <- 30                                # CLT, number of runs
-iterate_fit22 <- vector("list", n)
-
-for (i in 1: n) {
-  set.seed(7*i)                        # my lucky number is 7. 
-  iterate_fit22[[i]] <- nmf(merged_mat, k = 22, verbose = FALSE)
-  
-  rownames(iterate_fit22[[i]]$w) <- rownames(merged_mat)
-  colnames(iterate_fit22[[i]]$h) <- colnames(merged_mat)
-}
-
-# pair-wise comparison against reference
-W_list <- lapply(iterate_fit22, function(x) x$w)
-W_ref <- W_list[[1]]        
-
-cor_matrix <- list()
-for (i in 1:n) {
-  W <- W_list[[i]]
-  cor_matrix[[i]] <- cor(W, W_ref)
-}
-
-# getting stability score (how it compares)
-stability <- sapply(cor_matrix, function(avg) {
-  mean(apply(avg, 1, max))            # greedy algorithm matching
-})
-
-# bar graph for similarity. 0.75 as threshold 
-run_num <- 1:length(stability)
-bar_colors <- c("#9678B6", rep("lightgray", 29))
-
-barplot(stability,
-        names.arg = run_num,
-        col = bar_colors,
-        ylim = c(0, 1),
-        xlab = "NMF run",
-        ylab = "proportion similar",
-        main = "NMF similarity across 30 runs (k = 22), reference column 1")
-par(cex.main = 0.75)
-par(cex.axis = 0.5)
-abline(h = 0.75, col = "blue", lwd = 2, lty =2)
-
-
-# -----------------------------------------------------------------------------
-# 4. NMF on optimal k rank (22) on combined samples 
-fit22 <- nmf(merged_mat, k = 22, verbose = FALSE)
+# 4. final model, optimal rank was 10
+best_mod <- nmf(merged_mat, k = 10, seed = 42)
 
 # re-aligning row and column names (lost before)
-rownames(fit22$w) <- rownames(merged_mat)
-colnames(fit22$h) <- colnames(merged_mat)
+rownames(best_mod$w) <- rownames(merged_mat)
+colnames(best_mod$h) <- colnames(merged_mat)
 
 # renaming for ease of reference
-W <- fit22$w
-H <- fit22$h  
+W <- best_mod$w
+H <- best_mod$h  
 
-                 
+
 # -----------------------------------------------------------------------------
-# 5. viewing if the factors are orthogonal. using cosine similarity btwn vectors
+# 5. factors are orthogonality. using cosine similarity btwn factors
 library(proxyC)
 factor_sim <- simil(
-  t(W),                        # required to transpose to get factor x gene
+  t(W),                                            # required to transpose to get factor x gene
   method = "cosine"
 )
 
-library(corrplot)
-corrplot(
-  as.matrix(factor_sim),
-  method = "color",
-  type = "upper"
-)
+library(pheatmap)
+pheatmap(factor_sim, 
+         clustering_distance_rows = "correlation",
+         clustering_distance_cols = "correlation",
 
-
+         
 # -----------------------------------------------------------------------------
 # 6. finalizing, binding spatial data
 pd1_vis_coords <- readRDS(file.path(datadir, "pd1_vis_coords.rds"))
@@ -174,13 +127,10 @@ iso_vis_coords <- readRDS(datadir, "iso_vis_coords.rds")
 rownames(pd1_vis_coords)  <- paste0("pd1_", rownames(pd1_vis_coords))
 rownames(iso_vis_coords) <- paste0("iso_", rownames(iso_vis_coords))
 
-tH <- t(H)   # required to transpose to line up
+tH <- t(H)   # required to transpose to line up. colnamesH holds both
 
 common_pd1 <- rownames(pd1_vis_coords)[
   rownames(pd1_vis_coords) %in% colnames(H)
-]
-common_iso <- rownames(iso_vis_coords)[
-  rownames(iso_vis_coords) %in% colnames(H)
 ]
 
 pd1_joined <- cbind(
@@ -188,17 +138,11 @@ pd1_joined <- cbind(
   tH[common_pd1, , drop = FALSE]
 )
 
-iso_joined <- cbind(
-  iso_vis_coords[common_iso, , drop = FALSE],
-  tH[common_iso, , drop = FALSE]
-)
-
 # scale from pixels to microns for euclidean spatial alignment
-pd1_joined$x <- pd1_joined$x / 1.5454                   # careful with overwriting.
-pd1_joined$y <- pd1_joined$y / 1.5454
+pd1_joined$x <- pd1_joined$x / 1.5454              # careful with overwriting.
+pd1_joined$y <- pd1_joined$y / 1.5454              
 
-# export
-write.csv(iso_joined, file.path(datadir, "0525_NMF_iso.csv"))
+# export, repeating above for isotype control
+write.csv(pd1_joined, file.path(datadir, "0611_NMFs.csv"))
 
-ds_merged <- rbind(pd1_joined, iso_joined)              # for later optional use
-
+         
