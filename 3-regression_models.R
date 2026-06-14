9eccb200b751c374d31bdd21c0477facca292cd9
Sophie Liu <saliu@mdanderson.org>
2026-06-14 11:09:27 -0500

    Update 3-regression_models.R

--- 3-regression_models.R
+++ 3-regression_models.R
@@ -47,12 +47,12 @@ write.csv(head(df_weighted, 40), file.path(localdir, "factor_weights40.csv"),
 # - know data is nonlinear and non-normal
 # - means smoothing need to incorporate density covariate
 # - better but still non-orthogonal when using NMF. raw counts, per-gene weights
-# - attempted to restore pseudo-independence by sampling non-overlapping disks
+# - pseudo-independence by sampling non-overlapping disks
 
 
 # -----------------------------------------------------------------------------
 # 1. setting vars and family
-x <- as.matrix(df[, fact_cols])         # 2x repeated control condition & treated
+x <- as.matrix(df[, fact_cols])         
 x_scaled <- scale(x)                    # visible representation of RNA counts. z-scores
 
 y_binar <- df$exist_dying               
