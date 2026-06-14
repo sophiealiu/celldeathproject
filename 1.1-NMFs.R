277bf19405eaed7a061e9fc27928afe142cdade8
Sophie Liu <saliu@mdanderson.org>
2026-06-14 12:57:02 -0500

    Update 1.1-NMFs.R

--- 1.1-NMFs.R
+++ 1.1-NMFs.R
@@ -71,7 +71,8 @@ for (i in seq_along(ks)) {
 mean_loss <- rowMeans(loss_matrix)
 k_opt <- uik(x = ks, y = mean_loss)
 
-# b. stability score using Hungarian algorithm. result: 0.9498465 yay close to 1
+# b. stability score using Hungarian algorithm. result: 0.9498465
+# gene weights across regeneration of factors is lined up to check that stochasticity still produces stable results
 library(clue)
 nruns_stab <- 30                                   # here CLT
 pair_stab <- c()
