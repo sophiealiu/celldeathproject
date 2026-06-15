e19d62df5a6454ca28542e8565577fa88f9eb3ec
Sophie Liu <saliu@mdanderson.org>
2026-06-15 16:02:33 -0500

    Update 3-regression_models.R

--- 3-regression_models.R
+++ 3-regression_models.R
@@ -1,41 +1,63 @@
 # -----------------------------------------------------------------------------
 # Author: Sophie A. Liu
-# Date : 06/14/2026 10:01pm MDT
-# Purpose: model iterations, regression dying tumor ~ factors
+# Date : 06/15/2026 4:02pm MDT
+# Purpose: model iterations, regression dying tumor ~ NMF factors
 # -----------------------------------------------------------------------------
 
 library(dplyr)
+library(ggplot2)
 library(tidyr)
 
-# importing files
-iso <- read.csv("9NMF_iso40.csv")
-# iso <- read.csv("9NSF_iso40.csv")
-pd1 <- read.csv("9NMF_pd140.csv")
-# iso <- read.csv("9NSF_pd140.csv")
-
+localdir <- "C:/Users/saliu/OneDrive - Inside MD Anderson/Documents/Cell Death runover/data"
+iso <- read.csv(file = file.path(localdir, "0613_9NMF_iso40.csv"))
+pd1 <- read.csv(file = file.path(localdir, "0614_9NMF_pd140.csv"))
 df <- rbind(iso, pd1)
 
-
 # -----------------------------------------------------------------------------
+# preliminary checks of NMF factors
+fact_cols <- names(df)[10:18]                         # indices, check head first
+
+sorted_genes <- (sort(W[,9], decreasing = TRUE))      # summary of top genes
+head(names(sorted_genes), 10)
+
+sorted_factor <- list()
+for (i in 1:length(fact_cols)) {
+  ord <- order(W[, i], decreasing = TRUE)             # re-import gene factors
+  
+  sorted_factor[[i]] <- data.frame(
+    gene   = rownames(W)[ord],
+    weight = W[ord, i]
+  )
+  
+  colnames(sorted_factor[[i]]) <- c(
+    paste0("factor_", fact_cols[i], "_gene"),
+    paste0("factor_", fact_cols[i], "_weight")
+  )
+}
+
+df_weighted <- do.call(cbind, sorted_factor)
+write.csv(head(df_weighted, 40), file.path(localdir, "factor_weights40.csv"),
+          rownames = FALSE)
+
+# ----------------------------------------------------------------------------_
 # BEGINNING MODELING
 
 # What are our assumptions?
 # - know data is nonlinear and non-normal
-# - means smoothing, need to incorporate tumor density covariate
-# - better but still non-orthogonal when using NMF. using raw counts w/ per-gene weights
-# - pseudo-independence by sampling non-overlapping disks
+# - means smoothing need to incorporate density covariate
+# - better but still non-orthogonal when using NMF. raw counts, per-gene weights
+# - attempted to restore pseudo-independence by sampling non-overlapping disks
 
 
 # -----------------------------------------------------------------------------
 # 1. setting vars and family
-fact_cols <- names(df)[grepl("^X", names(df))]                        
-x <- as.matrix(df[, fact_cols])         
+x <- as.matrix(df[, fact_cols])         # 2x repeated control condition & treated
 x_scaled <- scale(x)                    # visible representation of RNA counts. z-scores
 
-y_binar <- df$exist_dying               
+y_binar <- df$exist_dying               # binomial  [X]
 y_disc <- df$n_dying                    # overdispersion -> negative binomial
                                         # zero inflation overfit from performance stats
-trt <- ifelse(df$sample == "apd1", 1, 0)
+trt <- ifelse(df$sample == "pd1-9", 1, 0)
 tum_off <- offset(log(df$n_tumor+1))    # offset term, don't treat same as coeffs
 
 
@@ -55,35 +77,36 @@ ggplot(OR_clean %>%
 
 
 # -----------------------------------------------------------------------------
-# 3. using counts considers sequencing depth. iterate after dx in 5
+# 3. using counts considers sequencing depth. iterate after diagnosis in 5
 hist(y_disc, 
      breaks = 30,
      main = "Distribution of count dying",
      xlab = "number of dying cells in 40 micron vicinity")
 
-# a. negative binomial. AIC = 4390.5
+
+# a. negative binomial. AIC = 4410.3
 library(MASS)
 nb <- glm.nb(y_disc ~ df$n_immune + trt + x_scaled + tum_off)
 
-p_vals <- sort(                                   # p first visualization
+p_vals <- sort(                                   # first visualization
   coef(summary(nb))[, "Pr(>|z|)"], 
   decreasing = FALSE)
-p_vals_adj <- p.adjust(p_vals, method = "BH")     # benjamini still optimistic
+p_vals_adj <- p.adjust(p_vals, method = "BH")     # after benjamini still optimistic
 
-# b. zero-inflation. AIC = 4459.6
+# b. zero-inflation. AIC = 4428.1
 library(glmmTMB)
 zero <- glmmTMB(y_disc ~ n_immune + trt + x_scaled, 
                 data = df, 
                 family = nbinom2)
 
-# c. geographical trends, Moran's I. Not combined w/ zero. AIC = 4358.5
+# c. geographical trends, Moran's I. Not combined w/ zero. AIC = 4342.3
 library(mgcv)
 spatial <- gam(y_disc ~ n_immune + trt + x_scaled + tum_off + 
                  s(cx, cy, bs = "gp"),     # spatial smoothing
                 data = df,                         
                 family = nb())           
 
-# d. reduced model, only "significant" predictors. AIC = 4356.9
+# d. reduced model, only "significant" predictors. AIC = 4354.5
 red_col <- fact_cols[c(1, 4)]
 x_red <- as.matrix(df[, red_col])
 x_red_sc <- scale(x_red)
@@ -104,20 +127,27 @@ elastic <- glmnet(df$n_immune + trt + x_scaled + tum_off, y_disc,
                   alpha = 0.5, lambda = optim)
 coef(elastic) 
 
+# f. interaction (careful overfitting). AIC = 4314.8
+inter1 <- scale(df$X1)*trt
+inter4 <- scale(df$X4)*trt
+sr_int <- gam(y_disc ~ n_immune + trt + x_red_sc + tum_off + inter1 + inter4
+                     + s(cx, cy, bs = "gp"),
+                   data = df,
+                   family = nb())
 
 # -----------------------------------------------------------------------------
 # 5. diagnostic stats
 library(DHARMa)
-sim_resR <- simulateResiduals(spatial_red)
-plot(sim_res)
+sim_resR <- simulateResiduals(sr_int)
+plot(sim_resR)
 
 # ratio of zeroes, okay need to do this.
 testZeroInflation(sim_res)
 
 # finding the culprit throwing off my data
-full_vars <- cbind(x_red_sc, df$n_immune, trt)
-for(i in 1:4) {                                   # 4 total reduced factors
-  plotResiduals(sim_res, full_vars[, i],
+full_vars <- cbind(x_red_sc, trt)
+for(i in 1:4) {                                   # number of reduced factors
+  plotResiduals(sim_resR, full_vars[, i],
                 main = colnames(full_vars)[i])
 }
 
@@ -125,8 +155,8 @@ testSpatialAutocorrelation(simulationOutput = sim_res0,
                            x = df$cx, 
                            y = df$cy)
 
-# c. visualization of treatment interaction
-ggplot(df, aes(x = scale(X1), y = y_disc,
+# c. visualization of treatment interaction. not that informative but potential
+ggplot(df10, aes(x = scale(X1), y = y_disc,
                color = factor(trt,
                               levels = c(0,1),
                               labels = c("control","treated")))) +
@@ -136,7 +166,7 @@ ggplot(df, aes(x = scale(X1), y = y_disc,
                "treated" = "pink"),
     name = NULL
   ) +
-  coord_cartesian(xlim = c(0, 1.645)) + 
+  coord_cartesian(xlim = c(0, 2)) + 
   labs(
     x = "factor 1 expression (standardized)",
     y = "counts dying in 40 micron radius",
@@ -153,21 +183,45 @@ nb_mess <- glm.nb(y_messUp ~ df$n_immune + trt + x_scaled + tum_off)
 
 
 # -----------------------------------------------------------------------------
-# 7. radii sensitivity (repeated for control and experimental) 3x
-# kept it nonreduced to see if anything else pops out 
-df10 <- read.csv("9NMF_pd110.csv")
-df20 <- read.csv("9NMF_pd120.csv")  
-df80 <- read.csv("9NMF_pd180.csv") 
+# 7. vasculature for verification. cell type proximity
+iso10 <- read.csv(file = file.path(localdir, "0614_9NMF_iso10.csv"))
+pd110 <- read.csv(file = file.path(localdir, "0614_9NMF_pd110.csv"))
+df10 <- rbind(iso10, pd110)
 
-x10_sc <- scale(as.matrix(df10[, fact_col]))        
-y10_disc <- d10f$n_dying
-
-trt10 <- ifelse(df10$sample == "apd1", 1, 0)
+x10 <- as.matrix(df10[, fact_cols])
+x10sc <- scale(x10)
+trt10 <- ifelse(df10$sample == "pd1-9", 1, 0)
 tum10_off <- offset(log(df10$n_tumor+1)) 
 
-spatial_red10 <- gam(y10_disc ~ n_immune + trt10 + x10_sc + tum10_off +
-                     s(cx, cy, bs = "gp"),
-                   data = df10,
-                   family = nb())
+yd_test <- df10$n_lectin            
+nb_test <- glm.nb(yd_test ~ x10sc)
+
+y10d <- df10$n_dying
+nb10 <- glm.nb(y10d ~ df10$n_immune + trt + tum10_off+ x10sc)
 
 
+# -----------------------------------------------------------------------------
+# 8. more radii sensitivity
+iso20 <- read.csv(file = file.path(localdir, "0614_9NMF_iso20.csv"))
+pd120 <- read.csv(file = file.path(localdir, "0614_9NMF_pd120.csv"))
+df20 <- rbind(iso20, pd120)
+
+iso80 <- read.csv(file = file.path(localdir, "0614_9NMF_iso80.csv"))
+pd180 <- read.csv(file = file.path(localdir, "0614_9NMF_pd180.csv"))
+df80 <- rbind(iso20, pd120)  
+
+x20 <- as.matrix(df20[, fact_cols])
+x20sc <- scale(x20)  
+trt20 <- ifelse(df20$sample == "pd1-9", 1, 0)
+tum20_off <- offset(log(df20$n_tumor+1)) 
+
+x80 <- as.matrix(df80[, fact_cols])
+x80sc <- scale(x80)  
+trt80 <- ifelse(df80$sample == "pd1-9", 1, 0)
+tum80_off <- offset(log(df80$n_tumor+1)) 
+
+y20d <- df20$n_dying
+y80d <- df80$n_dying
+
+nb20 <- glm.nb(y20d ~ df20$n_immune + trt20 + tum20_off+ x20sc)
+nb80 <- glm.nb(y80d ~ df80$n_immune + trt80 + tum80_off+ x80sc)
