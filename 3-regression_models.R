5e509bfea4a26a1f185143bfed2d6994073061bb
Sophie Liu <saliu@mdanderson.org>
2026-06-18 11:04:13 -0500

    Update 3-regression_models.R

--- 3-regression_models.R
+++ 3-regression_models.R
@@ -8,9 +8,30 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
+# 1.1
+# iso <- read.csv("sig_iso40.csv"))
+# pd1 <- read.csv("sig_pd140.csv"))
+# df <- rbind(iso, pd1)
+
+# iso <- read.csv("all_genes_iso40.csv"))
+# pd1 <- read.csv("all_genes_pd140.csv"))
+# df <- rbind(iso, pd1)
+
+# iso <- read.csv("DE_iso40.csv"))
+# pd1 <- read.csv("DE_pd140.csv"))
+# df <- rbind(iso, pd1)
+
+
+# 1.2 MAIN
 iso <- read.csv("9NMF_iso40.csv"))
 pd1 <- read.csv("9NMF_pd140.csv"))
 df <- rbind(iso, pd1)
+
+# 1.3
+# iso <- read.csv("9NSF_iso40.csv"))
+# pd1 <- read.csv("9NSF_pd140.csv"))
+# df <- rbind(iso, pd1)
+
 
 # -----------------------------------------------------------------------------
 # preliminary checks of NMF factors
