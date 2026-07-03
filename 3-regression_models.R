f79bd1f9d7f49ac03c02780d1d644656a9b7764e
Sophie Liu <saliu@mdanderson.org>
2026-07-02 22:03:41 -0500

    Update 3-regression_models.R

--- 3-regression_models.R
+++ 3-regression_models.R
@@ -11,13 +11,13 @@ library(tidyr)
 datadir <- "path/to/your/working/directory"
 
 # 1.2 MAIN
-iso <- read.csv("9NMF_iso40.csv"))
-pd1 <- read.csv("9NMF_pd140.csv"))
+iso <- read.csv(file.path(datadir,"9NMF_iso40.csv")))
+pd1 <- read.csv(file.path(datadir,"9NMF_pd140.csv")))
 df <- rbind(iso, pd1)
 
 # 1.3
-# iso <- read.csv("9NSF_iso40.csv"))
-# pd1 <- read.csv("9NSF_pd140.csv"))
+# iso <- read.csv(file.path(datadir,"9NSF_iso40.csv")))
+# pd1 <- read.csv(file.path(datadir,"9NSF_pd140.csv")))
 # df <- rbind(iso, pd1)
 
 
