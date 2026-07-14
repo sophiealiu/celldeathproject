2b197218ec05b9845e693ea57c7520e5bc6fe5dd
Sophie Liu <saliu@mdanderson.org>
2026-07-14 10:19:54 -0500

    Update 4-graphics.R

--- 4-graphics.R
+++ 4-graphics.R
@@ -159,9 +159,7 @@ cor(pd1_dist$X1, pd1_dist$total_umis)
 ggplot(pd1_dist, aes(x = x, y = y, z = X9/total_umis)) +
   stat_summary_2d(binwidth = c(48, 48),
                   alpha = 1) +      
-  scale_fill_viridis_c(
-    option = "magma"
-  ) +
+  scale_fill_viridis_c(option = "magma") +
   
   geom_point(
     data  = filter(df_pd1IF, cell_type == "gc3ai"),
