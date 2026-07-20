a7c0631884484f927b0d004bcd5bef7857446393
Sophie Liu <saliu@mdanderson.org>
2026-07-20 16:38:38 -0500

    Update 1-NMF.R

--- 1-NMF.R
+++ 1-NMF.R
@@ -13,7 +13,7 @@ library(Seurat)
 
 # importing files. no assumptions made, hence raw data. 10x Genomics recommended 8 micron bins
 # datadir <- "path/to/your/working/directory"
-datadir <- ""I:\Hu Lab\Sophie\1. Cell death\all final data"
+datadir <- "I:\Hu Lab\Sophie\1. Cell death\all final data"
 
 iso_raw <- readRDS(file.path(datadir,"iso_raw.rds"))
 pd1_raw <- readRDS(file.path(datadir,"pd1_raw.rds"))
